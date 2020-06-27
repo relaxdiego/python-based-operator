468fe8e693692a2c4a8877ace20de323f6061913
Mark Maglana <mark.maglana@canonical.com>
2020-06-27 08:29:42 +0000

    Watch only for pod events and do an endless loop

--- prometheus_operator/operator.py
+++ prometheus_operator/operator.py
@@ -20,25 +20,17 @@ def main():
 
     v1 = client.CoreV1Api()
 
-    count = 10
-    w = watch.Watch()
-    for event in w.stream(v1.list_namespace, timeout_seconds=10):
-        print("Event: %s %s" % (event['type'], event['object'].metadata.name))
-        count -= 1
-        if not count:
-            w.stop()
-    print("Finished namespace stream.")
+    while True:
+        w = watch.Watch()
 
-    for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds=10):
-        print("Event: %s %s %s" % (
-            event['type'],
-            event['object'].kind,
-            event['object'].metadata.name)
-        )
-        count -= 1
-        if not count:
-            w.stop()
-    print("Finished pod stream.")
+        log.debug("Watching pod events in all namespaces")
+        for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds=10):
+            log.debug("Event: %s %s %s" % (
+                event['type'],
+                event['object'].kind,
+                event['object'].metadata.name)
+            )
+        log.debug("Finished pod stream. Retrying")
 
 
 if __name__ == "__main__":
